069ea20d0998bdba11a7b6c82b3ff2f9bd798f01
Kevin Brannan <brannake@gmail.com>
2017-09-26 14:03:35 -0700

    Fixes front end alignment details

--- app/components/HeatMaps.js
+++ app/components/HeatMaps.js
@@ -13,13 +13,13 @@ class HeatMaps extends Component {
     imageData: [],
     loaded: false,
     data: [
-        {name: 'March', uv: 400, pv: 240, amt: 240},
-        {name: 'April', uv: 300, pv: 138, amt: 221},
-        {name: 'May', uv: 200, pv: 980, amt: 229},
-        {name: 'June', uv: 278, pv: 398, amt: 200},
-        {name: 'July', uv: 189, pv: 480, amt: 218},
-        {name: 'August', uv: 239, pv: 380, amt: 250},
-        {name: 'September', uv: 349, pv: 430, amt: 210},
+        {name: 'March', uv: 40, pv: 240, amt: 240},
+        {name: 'April', uv: 30, pv: 138, amt: 221},
+        {name: 'May', uv: 20, pv: 980, amt: 229},
+        {name: 'June', uv: 28, pv: 398, amt: 200},
+        {name: 'July', uv: 19, pv: 480, amt: 218},
+        {name: 'August', uv: 29, pv: 380, amt: 250},
+        {name: 'September', uv: 49, pv: 430, amt: 210},
       ]
   };
 
@@ -33,10 +33,10 @@ class HeatMaps extends Component {
         <div id="chart-container">
           <LineChart 
             id="lineChart"
-            width={800} 
-            height={400} 
+            width={940} 
+            height={500} 
             data={this.state.data} 
-            margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+          >
           <Line type="monotone" dataKey="uv" stroke="#8884d8" />
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
           <XAxis dataKey="name" />

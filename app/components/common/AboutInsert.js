81b9fe16132d204e3285b14400315f60cda4df4b
Kevin Brannan <brannake@gmail.com>
2017-12-29 18:47:45 -0800

    Adds readme and few others things

--- app/components/common/AboutInsert.js
+++ app/components/common/AboutInsert.js
@@ -3,22 +3,24 @@ import React from "react";
 const AboutInsert = () => (
   <div className="container" id="about-container">
     <div className="row">
-        <p id="help">
-            Welcome to Introspectiv!
-        </p>
-        <p2>
-            The idea behind Introspectiv is simple: you have room to grow
-            in understanding yourself and what makes you happy.
-
-            This app is a photo journal plugged into the IBM Watson supercomputer that
-            analyzes your journal entries for emotional content.
-
-            You can track changes in your mood over time, and find associations
-            between changes in your mood and events described in your journal
-            entries.
-
+        <div id="help">
+            <p>
+                The idea behind Introspectiv is simple: you have room to grow
+                in understanding yourself and what makes you happy.
+            </p>
+            <p>
+                This app is a photo journal plugged into the IBM Watson supercomputer that
+                analyzes your journal entries for emotional content.
+            </p>
+            <p>
+                You can track changes in your mood over time, and find associations
+                between changes in your mood and events described in your journal
+                entries.
+            </p>
+            <p>
             Please see the videos below for instructions on how to access various features.
-        </p2>
+            </p>
+        </div>
     </div>
         <div className="row">
         </div>

f63757e686f4c4188f61ab4beb3539ea2a3387e7
Kevin Brannan <brannake@gmail.com>
2018-01-21 14:18:40 -0800

    Revises db params for heroku

--- models/index.js
+++ models/index.js
@@ -20,7 +20,10 @@ var sequelize = new Sequelize("tm7hodyik26q7v83", "dy388znrr3l2meea", "d6io3wu5d
  },
 });
 
-var sequelize = new Sequelize("Introspectiv_db", "dy388znrr3l2meea", "d6io3wu5dq7wkptn", "mysql");
+var sequelize = new Sequelize("Introspectiv_db", "dy388znrr3l2meea", "d6io3wu5dq7wkptn", {
+  host: "127.0.0.1",
+  dialect: 'mysql'
+  });
 
 fs
   .readdirSync(__dirname)
